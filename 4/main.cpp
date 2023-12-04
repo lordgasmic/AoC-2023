693145ce2fac64632a443e4acc98769ec1a0b5bc
Nick Wellman <wellman.nicholas@gmail.com>
2023-12-04 15:35:26 -0500

    part 2 working

--- 4/main.cpp
+++ 4/main.cpp
@@ -26,6 +26,19 @@ void readFile(std::vector<std::string> &lines) {
     }
 }
 
+void parseNumbers(std::string &line, std::vector<int> &winningNumbers, std::vector<int> &playingNumbers) {
+    auto temp = line.substr(env.headerLength);
+    for (int i = 0; i< env.winningCols; i++) {
+        auto num = temp.substr(i*3, 3);
+        winningNumbers.insert(winningNumbers.end(), std::stoi(num));
+    }
+    for (int i = 0; i< env.playCols; i++) {
+        int pipe = temp.find_first_of('|');
+        auto num = temp.substr(pipe + 1 + (i*3), 3);
+        playingNumbers.insert(playingNumbers.end(), std::stoi(num));
+    }
+}
+
 void partOne() {
     long sum{0};
     std::vector<std::string> lines;
@@ -34,16 +47,7 @@ void partOne() {
     for(std::string &line : lines){
         std::vector<int> winningNumbers;
         std::vector<int> playingNumbers;
-        auto temp = line.substr(env.headerLength);
-        for (int i = 0; i< env.winningCols; i++) {
-            auto num = temp.substr(i*3, 3);
-            winningNumbers.insert(winningNumbers.end(), std::stoi(num));
-        }
-        for (int i = 0; i< env.playCols; i++) {
-            int pipe = temp.find_first_of('|');
-            auto num = temp.substr(pipe + 1 + (i*3), 3);
-            playingNumbers.insert(playingNumbers.end(), std::stoi(num));
-        }
+        parseNumbers(line, winningNumbers, playingNumbers);
 
         int matchedNumbers{0};
         for (int num : playingNumbers) {
@@ -58,7 +62,37 @@ void partOne() {
 
     std::cout << "part one sum is: " << sum << std::endl;
 }
+
+void lolsRecursion(std::vector<std::string> &lines, int pos, int &sum) {
+    std::vector<int> winningNumbers;
+    std::vector<int> playingNumbers;
+    parseNumbers(lines.at(pos), winningNumbers, playingNumbers);
+
+    sum++;
+    int matchedNumbers{0};
+    for (int num : playingNumbers) {
+        if (std::count(winningNumbers.begin(), winningNumbers.end(), num)){
+            matchedNumbers++;
+            lolsRecursion(lines, pos + matchedNumbers, sum);
+        }
+    }
+}
+
+void partTwo() {
+    int sum{0};
+    std::vector<std::string> lines;
+    readFile(lines);
+
+    for(int i =  0; i < lines.size(); i++){
+        lolsRecursion(lines, i, sum);
+    }
+
+    std::cout << "part one sum is: " << sum << std::endl;
+}
+
 int main() {
     partOne();
+    partTwo();
+
     return 0;
 }
