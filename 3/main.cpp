21d7f46d63f3d7db4bf512c68dbd891d87590a14
Nick Wellman <wellman.nicholas@gmail.com>
2023-12-04 14:09:53 -0500

    part 2 working

--- 3/main.cpp
+++ 3/main.cpp
@@ -143,41 +143,122 @@ void partOne() {
     std::cout << "part one sum: " << sum << std::endl;
 }
 
-//void partOne() {
-//    int sum{0};
-//    std::vector<std::string> lines;
-//    std::ifstream infile;
-//    infile.open("/home/lordgasmic/workspace/AoC-2023/3/input.txt");
-//    if (infile.is_open()) {
-//        std::string str;
-//        while (std::getline(infile, str)) {
-//            lines.insert(lines.end(), str);
-//        }
-//        infile.close();
-//    }
-//
-//    for (int lineIndex = 1; lineIndex < lines.size(); lineIndex++) {
-//        std::string previous = lines.at(lineIndex - 1);
-//        std::string current = lines.at(lineIndex);
-//
-//        for (int i = 0; i < current.length(); i++) {
-//            std::string single = current.substr(i, 1);
-//            if (!std::regex_match(single, matchDigit) && !std::regex_match(single, matchDot)) {
-//                // check TL TC TR (index - 1, i - 1)(index - 1, i)(index -1, i + 1)
-//                // check L      R (index, i - 1) (index, i + 1)
-//            }
-//        }
-//    }
-//
-//    std::cout << "part one sum: " << sum << std::endl;
-//}
-
 void partTwo() {
+    int sum{0};
+    std::vector<LineData *> parsedParts[NUM_LINES];
+    int index{0};
+    std::ifstream infile;
+    infile.open("/home/lordgasmic/workspace/AoC-2023/3/input.txt");
+    if (infile.is_open()) {
+        std::string str;
+        while (std::getline(infile, str)) {
+            std::string accumulator;
+            auto *lineData = new LineData();
+            for (int i = 0; i < str.length(); i++) {
+                std::string single = str.substr(i, 1);
+                if (single == ".") {
+                    if (accumulator.length() != 0) {
+                        lineData->m_value = accumulator;
+                        lineData->m_upperRange = i;
+                        parsedParts[index].insert(parsedParts[index].end(), lineData);
+                        accumulator = "";
+                        lineData = new LineData();
+                    }
+                    continue;
+                }
+                if (std::regex_match(single, matchDigit)) {
+                    if (accumulator.length() == 0) {
+                        lineData->m_position = i;
+                        lineData->m_dataType = part;
+                        lineData->m_lowerRange = i - 1;
+                    }
+                    accumulator += single;
+                }
+                else {
+                    if (accumulator.length() == 0) {
+                        lineData->m_value = single;
+                        lineData->m_position = i;
+                        lineData->m_dataType = symbol;
+                    }
+                    else {
+                        lineData->m_value = accumulator;
+                        lineData->m_upperRange = i;
+                        parsedParts[index].insert(parsedParts[index].end(), lineData);
+                        accumulator = "";
+                        lineData = new LineData();
+                        lineData->m_value = single;
+                        lineData->m_position = i;
+                        lineData->m_dataType = symbol;
+                    }
+                    parsedParts[index].insert(parsedParts[index].end(), lineData);
+                    lineData = new LineData();
+                }
+            }
 
+            if (accumulator.length() != 0) {
+                lineData->m_value = accumulator;
+                lineData->m_upperRange = str.length() - accumulator.length() - 1;
+                parsedParts[index].insert(parsedParts[index].end(), lineData);
+                accumulator = "";
+                lineData = new LineData();
+            }
+
+            index++;
+        }
+        infile.close();
+    }
+
+    index = 1;
+    while (index < NUM_LINES - 1) {
+        std::vector top = parsedParts[index - 1];
+        std::vector current = parsedParts[index];
+        std::vector bottom = parsedParts[index + 1];
+
+        // compare current to current
+        for (int i = 0; i < current.size(); i++) {
+            auto currentPart = current.at(i);
+            if (currentPart->m_dataType == symbol && currentPart->m_value == "*") {
+                std::vector<LineData*> adjacents;
+                if (i > 0) {
+                    auto temp = current.at(i - 1);
+                    if (temp->m_upperRange == currentPart->m_position) {
+                        adjacents.insert(adjacents.end(), temp);
+                    }
+                }
+                if (i + 1 < current.size()) {
+                    auto temp = current.at(i + 1);
+                    if (currentPart->m_position == temp->m_lowerRange) {
+                        adjacents.insert(adjacents.end(), temp);
+                    }
+                }
+
+                for(LineData *searchPart : top){
+                    if (searchPart->m_dataType == part && searchPart->m_lowerRange <= currentPart->m_position &&
+                                                        currentPart->m_position <= searchPart->m_upperRange) {
+                        adjacents.insert(adjacents.end(), searchPart);
+                    }
+                }
+                for(LineData *searchPart : bottom){
+                    if (searchPart->m_dataType == part && searchPart->m_lowerRange <= currentPart->m_position &&
+                                                        currentPart->m_position <= searchPart->m_upperRange) {
+                        adjacents.insert(adjacents.end(), searchPart);
+                    }
+                }
+
+                if (adjacents.size() == 2) {
+                    sum += (std::stoi(adjacents.at(0)->m_value) * std::stoi(adjacents.at(1)->m_value));
+                }
+            }
+        }
+
+        index++;
+    }
+
+    std::cout << "part two sum: " << sum << std::endl;
 }
 
 int main() {
-    partOne();
+//    partOne();
     partTwo();
     return 0;
 }
